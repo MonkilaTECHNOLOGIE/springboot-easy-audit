d24d524ee0998e05a2defdfee93c6ed170393626
Joe Monkila <jnkiwa25@gmail.com>
2025-05-05 10:10:52 +0100

    init

---
+++ src/main/java/com/monkilatech/easy_audit/EasyAuditApplication.java
@@ -0,0 +1,13 @@
+package com.monkilatech.easy_audit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyAuditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyAuditApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/monkilatech/easy_audit/EasyAuditApplicationTests.java
@@ -0,0 +1,13 @@
+package com.monkilatech.easy_audit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasyAuditApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
