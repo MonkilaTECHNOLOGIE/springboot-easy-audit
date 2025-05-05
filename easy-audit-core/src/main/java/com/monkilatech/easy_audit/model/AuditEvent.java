95b96d06915927b94abd6d36612ba48808b4c03f
Joe Monkila <jnkiwa25@gmail.com>
2025-05-05 10:20:18 +0100

    easy_ audit core (model)

--- easy-audit-core/src/main/java/com/monkilatech/easy_audit/model/AuditEvent.java
+++ easy-audit-core/src/main/java/com/monkilatech/easy_audit/model/AuditEvent.java
@@ -1,3 +1,17 @@
+package com.easyaudit.easy_audit.model;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class AuditEvent {
-    
+    private String entity;
+    private String action;
+    private String username;
+    private LocalDateTime timestamp;
+    private Map<String, Object> changes;
 }
