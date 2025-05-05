42150d859c82c34a8b8bdc3e02f53e404a204514
Joe Monkila <jnkiwa25@gmail.com>
2025-05-05 14:09:01 +0100

    .

--- src/main/java/com/monkilatech/easy_audit/EasyAuditApplication.java
+++ src/main/java/com/monkilatech/easy_audit/EasyAuditApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EasyAuditApplication {
-
     
     public static void main(String[] args) {
         SpringApplication.run(EasyAuditApplication.class, args);
