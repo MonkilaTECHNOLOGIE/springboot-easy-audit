e73fd55af8f75e965da121d6fb4fb24206ef2646
Joe Monkila <jnkiwa25@gmail.com>
2025-05-05 10:21:12 +0100

    easy_ audit core (service)

--- easy-audit-core/src/main/java/com/monkilatech/easy_audit/service/AuditLogger.java
+++ easy-audit-core/src/main/java/com/monkilatech/easy_audit/service/AuditLogger.java
@@ -1,3 +1,7 @@
-public class AuditLogger {
-    
+package com.easyaudit.easy_audit.service;
+
+import com.easyaudit.core.model.AuditEvent;
+
+public interface AuditLogger {
+    void log(AuditEvent event);
 }
--- easy-audit-core/src/main/java/com/monkilatech/easy_audit/service/DefaultAuditLogger.java
+++ easy-audit-core/src/main/java/com/monkilatech/easy_audit/service/DefaultAuditLogger.java
@@ -1,3 +1,15 @@
-public class DefaultAuditLogger {
-    
+package com.easyaudit.easy_audit.service;
+
+import com.easyaudit.core.model.AuditEvent;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DefaultAuditLogger implements AuditLogger {
+
+    @Override
+    public void log(AuditEvent event) {
+        System.out.println("[AUDIT] " + event.getAction() +
+            " by " + event.getUsername() +
+            " on " + event.getEntity());
+    }
 }
